6608c3876752fbdd0bf24becd10168b5dbdab58c
HunterLM <hunter109@gmail.com>
2022-05-25 19:39:23 -0500

    config and server.js

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const { mongoose, connect, connection } = require('mongoose');
 
 // Wrap Mongoose around local connection to MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/fakeSocialDB', {
