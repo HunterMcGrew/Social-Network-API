4490886e3e0f2cb1ef056f34e13c273ae82e1852
HunterLM <hunter109@gmail.com>
2022-05-27 09:51:58 -0500

    commented on thought-Controller

---
+++ controllers/thought-Controller.js
@@ -0,0 +1,13 @@
+const { ObjectId } = require("mongoose").Types;
+const User = require("../models/User");
+const Thought = require("../models/Thought");
+
+module.exports = {
+
+    // create thought
+
+    // update thought (patch)
+
+    // delete thought
+
+}
